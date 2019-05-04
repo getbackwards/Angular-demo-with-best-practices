48a76c7f08931be0849bf47feaea3d303c07e357
tgray <tony.gray@trademe.co.nz>
2019-05-04 16:00:36 +1200

    removed stuff to get it working

--- src/app/shared/cards-list/cards-list.module.ts
+++ src/app/shared/cards-list/cards-list.module.ts
@@ -2,13 +2,12 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { AppMaterialModule } from '../app-material/app-material.module';
-import { CardsTableComponent } from './cards-list.component';
+// import { CardsTableComponent } from './cards-list.component';
 import { CardsComponent } from './cards/cards.component';
 import { ListComponent } from './list/list.component';
 
 @NgModule({
   imports: [CommonModule, AppMaterialModule, TranslateModule],
-  declarations: [CardsTableComponent, ListComponent, CardsComponent],
-  exports: [CardsTableComponent]
+  declarations: [ListComponent, CardsComponent]
 })
 export class CardListModule {}
